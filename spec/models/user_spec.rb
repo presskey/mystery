31746d0c14280884aabea701ccd286152b1b037f
Alex Gusev <hello.from.code@gmail.com>
2015-09-09 14:00:43 +0300

    Create user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe User do
+  
+  it { is_expected.to validate_presence_of(:username) }
+  it { is_expected.to validate_uniqueness_of(:username) }
+  it { is_expected.to validate_presence_of(:password) }
+
+end
