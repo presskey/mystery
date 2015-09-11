7dee5658890c59059fd75803f0dd5e74d55a76a0
Alex Gusev <hello.from.code@gmail.com>
2015-09-11 13:57:16 +0300

    Rescue from exceptions with proper HTTP response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   attr_reader :current_user
 
+  rescue_from(Pundit::NotAuthorizedError)         { head :forbidden }
+  rescue_from(ActiveRecord::RecordNotFound)       { head :not_found }
+  rescue_from(ActionController::ParameterMissing) { head :bad_request }
+
   before_action :authenticate
   after_action :verify_authorized, except: :index
 
